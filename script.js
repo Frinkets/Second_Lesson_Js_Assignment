519073b8629f493f235e916fb1aec7f7a8de9699
Abdulfattah <radythepopil@gmail.com>
2020-01-12 22:53:38 +0600

    add files

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+let money = prompt("Ваш бюджет на месяц?", "100$" );
+let time = prompt("Введите дату в формате YYYY-MM-DD", "" );
+
+console.log(money);
+console.log(time);
+
+let appData = {
+    allmoney: money,
+    timeData: time
+}
+
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", "100$" );
+let answer2 = prompt("Во сколько обойдется?", "" );
+
+expenses: {
+    answer1 : answer2
+}
+
+optionalExpenses: {
+
+}
+
+income : []
+
+
+
+
+
